e0134d319f986bc06932f4500c6095bd7e063cd3
arpit <arpitsahu789@gmail.com>
2023-03-19 01:09:18 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/authentication_pracitce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.authentication_pracitce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+  
+  
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase Auth Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+
+      },
+    );
+  }
+}
